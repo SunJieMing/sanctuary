c08638de05a7a3f341c82f5d6bbe3e22f11e3947
Ben Nelson <s.benjamin.nelson@gmail.com>
2014-08-25 20:48:52 -0700

    Angular test

--- controller.js
+++ controller.js
@@ -1,6 +1,10 @@
 app.controller('appController', function($scope){
   $scope.test = 'Hi there!';
+  var friendArray = [];
   $scope.submit = function() {
     $scope.data = $scope.test;
+    friendArray = $scope.test.split(',');
+    console.log(friendArray);
   }
+
 });
